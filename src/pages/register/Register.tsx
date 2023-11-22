041ca112543152baee77eba85cc7133a81e0f64d
Aleksa Cekić <aleksa.cekic8@gmail.com>
2023-11-22 21:50:28 +0100

    init

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button"
+const Register = () => {
+    return (
+        <div>
+            Register page
+            <Button>Click me</Button>
+        </div>
+    )
+}
+
+export default Register
